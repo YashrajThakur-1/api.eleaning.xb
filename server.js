15b83eb5dd151b05bfcbc6fec83ee9d700b0cb08
yashraj thakur <yashrajthakur009@gmail.com>
2024-08-26 12:54:44 +0530

    minor changes

--- server.js
+++ server.js
@@ -14,24 +14,13 @@ app.use(
     methods: ["POST", "GET", "PUT", "DELETE", "PATCH", "UPDATE"],
   })
 );
-app.set("views", path.join(__dirname, "views"));
-app.set("view engine", "ejs");
+
 app.use(express.static(path.join(__dirname, "uploads")));
 app.use(bodyParser.json());
 app.use(bodyParser.urlencoded({ extended: false }));
 
 // routes
 app.use("/api", userroutes);
-
-app.get("/", async (req, res) => {
-  const welcome = "Welcome To our Ejs Site";
-  res.render("welcome", { welcome }); // Corrected
-});
-
-app.get("/home", async (req, res) => {
-  const message = "yashraj Singh Thakur";
-  res.render("home", { message }); // Corrected
-});
 
 // Start the server
 const port = process.env.PORT || 3000;
